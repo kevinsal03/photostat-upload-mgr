3ab6df70da4be4ac41313c719e8a9023f219ad63
Kevinsal03 <kevinsal03@users.noreply.github.com>
2019-04-23 23:15:05 -0400

    changed to correct operator for string concat, really rusty at php

--- config.php
+++ config.php
@@ -1,5 +1,5 @@
 <?php
 #Configuration Script
 $env = file_get_contents('switches/env');
-$storage = "/mount/disk1/psup/" + $env + "/"; #Main File Storage
- ?>
+$storage = "/mount/disk1/psup/" . $env . "/"; #Main File Storage
+?>
