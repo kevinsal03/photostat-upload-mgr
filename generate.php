32e77275d6b55b7dfa30699afafb69730053d176
Kevinsal03 <kevinsal03@users.noreply.github.com>
2019-04-24 11:37:55 -0400

    generation test

--- generate.php
+++ generate.php
@@ -3,6 +3,24 @@ require 'config.php';
 
 #create new file upload point
 $objUUID = uniqid($env . "_");
+$objStatus = true;
 
 echo($objUUID);
+echo("<br />");
+
+$data = [
+  "uuid"=>$objUUID,
+  "status"=>$objStatus
+]
+
+$sql = "INSERT INTO " . $env . " (uuid, active, `created-at`)
+VALUES ('" . $objUUID . "'," . $objStatus . ", 'NOW()')";
+
+echo($sql);
+echo("<br />");
+
+
+#finally geneate the JSON object to return to client
+echo(json_encode($data));
+
  ?>
