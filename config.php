52eb6df1d6b7315d27835b58097cc96ad8f9a1e9
Kevinsal03 <kevinsal03@users.noreply.github.com>
2019-04-23 23:55:05 -0400

    mysql connection POSSIBLY working

--- config.php
+++ config.php
@@ -2,4 +2,17 @@
 #Configuration Script
 $env = trim(file_get_contents('switches/env'));
 $storage = "/mount/disk1/psup/" . $env . "/"; #Main File Storage
+
+$sqldb = 'psup';
+$sqlsrv = 'localhost';
+$sqluser = trim(getenv('mysqluser'));
+$sqlpass = trim(getenv('mysqlpass'));
+
+// Create connection
+$conn = new mysqli($sqlsrv, $sqluser, $sqlpass, $sqldb);
+
+if (mysqli_connect_errno())
+  {
+  die("Failed to connect to MySQL: " . mysqli_connect_error());
+  }
 ?>
